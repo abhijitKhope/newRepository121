00f36fb4deeaf267bdf64074fa2a2824d2f9bd05
abhijitKhope <96292342+abhijitKhope@users.noreply.github.com>
2022-02-02 13:58:02 +0530

    Update Testt.java

--- JenkinsDemo2/src/Testt.java
+++ JenkinsDemo2/src/Testt.java
@@ -3,6 +3,10 @@ public class Testt {
 
 	public static void main(String[] args) {
 		System.out.println("welcome to 3rd project of jenkins");
+		
+		System.out.println("welcome to 3rd project of jenkins");
+		
+		
 
 	}
 
