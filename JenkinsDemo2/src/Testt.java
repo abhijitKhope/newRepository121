5280129962d1a0c7479fc85857feb862c09cef51
abhi <abhi@abhi-PC>
2022-03-12 10:48:21 +0530

    hhh

--- JenkinsDemo2/src/Testt.java
+++ JenkinsDemo2/src/Testt.java
@@ -7,6 +7,7 @@ public class Testt {
 		System.out.println("welcome to 3rd project of jenkins");
 		System.out.println("welcome again");
 		System.out.println("wlecm u");
+		System.out.println("welcome");
 		
 		
 
